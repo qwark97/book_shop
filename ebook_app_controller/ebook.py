02b27185af1bd2de4e3f5fce26e92f7ada50d2ca
qwark97 <marcin.plata1.konta@gmail.com>
2020-01-21 21:16:16 +0100

    create CRUD controller for EBook model

---
+++ ebook_app_controller/ebook.py
@@ -0,0 +1,35 @@
+from ebook_app.models import EBook
+
+
+class EBookController:
+
+    def create(self, name, price, available=False, cover=''):
+        ebook = EBook(
+            name,
+            price,
+            available,
+            cover
+        )
+        ebook.save()
+
+    def read(self, _id):
+        ebook = EBook.objects.get(id=_id)
+        return ebook
+
+    def update(self, ebook, **kwargs):
+        white_list = ebook.attributes()
+        new_kwargs = {}
+        for attr, val in kwargs.items():
+            if attr in white_list:
+                new_kwargs.update(
+                    (attr, white_list[attr](val))
+                )
+
+        ebook.objects.update(**new_kwargs)
+
+    def delete(self, ebook):
+        EBook.delete(ebook)
+
+    def _add_to_dict(self, dct, key, val):
+        if val:
+            dct[key] = val
