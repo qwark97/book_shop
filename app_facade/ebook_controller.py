8742701d69747b487f7a66e8e4019967ec763494
qwark97 <marcin.plata1.konta@gmail.com>
2020-01-24 23:01:23 +0100

    create delete method of CRUD model

--- app_facade/ebook_controller.py
+++ app_facade/ebook_controller.py
@@ -3,7 +3,8 @@ from models.ebook import EBook
 
 class EBookController:
 
-    def create(self, name, price, available=False, cover='', save=True):
+    @staticmethod
+    def create(name, price, available=False, cover='', save=True):
         ebook = EBook(
             name,
             price,
@@ -14,15 +15,19 @@ class EBookController:
             ebook.insert()
         return ebook
 
-    def read(self, _id):
+    @staticmethod
+    def read(_id):
         return EBook.get(_id)
 
-    def update(self, _id, **kwargs):
-        ebook = self.read(_id)
+    @staticmethod
+    def update(_id, **kwargs):
+        ebook = EBookController.read(_id)
         for key, val in kwargs.items():
             if ebook.__dict__.get(key, None) is None: continue
             ebook.__setattr__(key, val)
         ebook.update_state()
 
-    def delete(self, _id):
-        ebook = self.read(_id)
+    @staticmethod
+    def delete(_id):
+        ebook = EBookController.read(_id)
+        ebook.remove()
